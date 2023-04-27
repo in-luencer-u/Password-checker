5b15ccb3cb73d0068a7d6363fd1d7cbe2699a46d
Kaliyan123 <96758317+Kaliyan123@users.noreply.github.com>
2023-04-27 20:46:17 +0530

    Create Intro.sh

---
+++ Intro.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+echo "Welcome to the password checker tool!"
+sleep 0.5
+echo "This tool uses a database of commonly-used passwords to check if a password is secure or not."
+sleep 0.5
+echo "The database is downloaded from the SecLists project on Github, which contains a collection of password lists."
+sleep 0.5
+echo "The tool uses the hashcat password cracking utility to compare a password against the database and determine its strength."
+
+echo ""
+sleep 0.5
+
+
+echo "The script will output whether the password is found in the database and how long it would take to crack."
+
+echo ""
+
+sleep 0.5
+
+echo "The installation script will download the password database and install any required dependencies."
+
+echo ""
+sleep 0.5
+
+echo "Thank you for using the password checker tool. Stay safe online!"
+
+echo "TO CONTINUE TO TOOL PRESS ANY KEY AND ENTER"
+read s 
+ toilet password checker 
+echo " what do you want to check "
+echo " 1.password strength 
+       2.create a password 
+       3.create a password with your name 
+       4.check your password in rockyou text file "
+read num
+echo "type the number of you want to doo"
+ 1) chmod +x Passcheck.sh
+   ./Passcheck.sh
+ 2) chmod +x Createpass.sh
+   ./Createpass.sh
+ 3) chmod +x Createpasswithname.sh
+   ./Createpasswithname.sh
+ 4) chmod +x Rockyou.sh
+   ./Rockyou.sh
+  
+       
+
+
+
+
