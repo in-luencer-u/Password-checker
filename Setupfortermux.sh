5cb5301242a61752b40c03b825a9893d5d13b8cf
Kaliyan123 <96758317+Kaliyan123@users.noreply.github.com>
2023-04-27 15:44:43 +0530

    Create Setupfortermux.sh

---
+++ Setupfortermux.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Install required dependencies
+
+pkg update
+
+pkg install -y openssl hashcat wget unzip
+
+# Download the password database (SecLists)
+
+wget https://github.com/danielmiessler/SecLists/archive/master.zip
+
+unzip master.zip
+
+cd SecLists-master/Passwords/
+
+# Optional: Extract rockyou.txt for faster password cracking
+
+unzip -j rockyou.txt.zip -d rockyou/
+
+echo "Setup complete. You can now use the password checker tool."
+
