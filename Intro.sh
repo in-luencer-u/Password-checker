40aad37f004798267194573c5cd471ac3f97d420
Kaliyan123 <96758317+Kaliyan123@users.noreply.github.com>
2023-07-17 14:18:48 +0530

    Update Intro.sh

--- Intro.sh
+++ Intro.sh
@@ -7,45 +7,49 @@ sleep 0.5
 echo "The database is downloaded from the SecLists project on Github, which contains a collection of password lists."
 sleep 0.5
 echo "The tool uses the hashcat password cracking utility to compare a password against the database and determine its strength."
-
 echo ""
 sleep 0.5
-
-
 echo "The script will output whether the password is found in the database and how long it would take to crack."
-
 echo ""
-
 sleep 0.5
-
 echo "The installation script will download the password database and install any required dependencies."
-
 echo ""
 sleep 0.5
-
 echo "Thank you for using the password checker tool. Stay safe online!"
 
-echo "TO CONTINUE TO TOOL PRESS ANY KEY AND ENTER"
-read s 
- toilet password checker 
-echo " what do you want to check "
-echo " 1.password strength 
-       2.create a password 
-       3.create a password with your name 
-       4.check your password in rockyou text file "
+echo "TO CONTINUE TO TOOL, PRESS ANY KEY AND ENTER"
+read s
+
+toilet password checker
+
+echo "What do you want to do?"
+echo "1. Check password strength"
+echo "2. Create a password"
+echo "3. Create a password with your name"
+echo "4. Check your password in rockyou text file"
+
 read num
-echo "type the number of you want to doo"
- 1) chmod +x Passcheck.sh
-   ./Passcheck.sh
- 2) chmod +x Createpass.sh
-   ./Createpass.sh
- 3) chmod +x Createpasswithname.sh
-   ./Createpasswithname.sh
- 4) chmod +x Rockyou.sh
-   ./Rockyou.sh
-  
-       
 
+echo "Type the number of the option you want to choose:"
 
-
-
+case $num in
+  1)
+    chmod +x Passcheck.sh
+    ./Passcheck.sh
+    ;;
+  2)
+    chmod +x Createpass.sh
+    ./Createpass.sh
+    ;;
+  3)
+    chmod +x Createpasswithname.sh
+    ./Createpasswithname.sh
+    ;;
+  4)
+    chmod +x Rockyou.sh
+    ./Rockyou.sh
+    ;;
+  *)
+    echo "Invalid option. Exiting..."
+    ;;
+esac
